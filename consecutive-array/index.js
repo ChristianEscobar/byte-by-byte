91fb4a1fbab748831f9f6f07bc91a3031fcd3886
Christian Escobar <crs.escobar@gmail.com>
2020-04-05 20:28:06 -0700

    add more sample runs

--- consecutive-array/index.js
+++ consecutive-array/index.js
@@ -1,5 +1,15 @@
 const { consecutive } = require('./consecutive');
+const { samsAnswer } = require('./sams-answer');
 
+/*
 console.log(consecutive([4, 2, 1, 6, 5]));
 console.log(consecutive([4, 2, 1, 6, 3]));
 console.log(consecutive([5, 5, 3, 1]));
+console.log(
+	consecutive([2, 5, 3, 1, 10, 11, 12, 13, 14, 18, 19, 21, 23, 24, 30])
+);
+console.log(consecutive([1, 2, 3, 3, 4]));
+*/
+
+console.log(consecutive([4, 2, 1, 6, 5]));
+console.log(samsAnswer([4, 2, 1, 6, 5]));
